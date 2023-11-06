d611993d05997866873aae9090c3d107ee8f1802
Yusuph Omari <yousufummar649@gmail.com>
2023-11-07 00:18:30 +0300

    commrnting the superuser

--- app/faculty/page.tsx
+++ app/faculty/page.tsx
@@ -1,9 +1,114 @@
-import React from 'react'
+'use client';
+
+import { useAuth } from '@/providers/authProvider';
+import { fetchFacultyForms } from '@/utils/apiCalls';
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from '@nextui-org/react';
+import React, { useEffect, useState } from 'react';
+
+const rows = [
+  {
+    key: '1',
+    name: 'Tony Reichert',
+    role: 'CEO',
+    status: 'Active',
+  },
+  {
+    key: '2',
+    name: 'Zoey Lang',
+    role: 'Technical Lead',
+    status: 'Paused',
+  },
+  {
+    key: '3',
+    name: 'Jane Fisher',
+    role: 'Senior Developer',
+    status: 'Active',
+  },
+  {
+    key: '4',
+    name: 'William Howard',
+    role: 'Community Manager',
+    status: 'Vacation',
+  },
+];
+
+const columns = [
+  {
+    key: 'facultY_id',
+    label: 'FACULTY ID',
+  },
+  {
+    key: 'user',
+    label: 'USER',
+  },
+  {
+    key: 'duty',
+    label: 'DUTY',
+  },
+  {
+    key: 'reason',
+    label: 'REASON',
+  },
+  {
+    key: 'subneeded',
+    label: 'SUB NEEDED',
+  },
+  {
+    key: 'time',
+    label: 'TIME',
+  },
+  {
+    key: 'status',
+    label: 'STATUS',
+  },
+];
 
 const FacultyPage = () => {
-  return (
-    <div>FacultyPage</div>
-  )
-}
+  const [forms, setForms] = useState([]);
+  const { accessToken } = useAuth();
 
-export default FacultyPage
+  console.log(accessToken);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchFacultyForms(accessToken);
+        console.log(data)
+        setForms(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [accessToken]);
+
+  console.log(forms);
+
+  return (
+    <Table aria-label="Example table with dynamic content">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => (
+              <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default FacultyPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 'use client';
 
-import NextLink from 'next/link';
-import { Link } from '@nextui-org/link';
-import { Snippet } from '@nextui-org/snippet';
-import { Code } from '@nextui-org/code';
-import { button as buttonStyles } from '@nextui-org/theme';
-import { siteConfig } from '@/config/site';
-import { title, subtitle } from '@/components/primitives';
-import { GithubIcon } from '@/components/icons';
 import FacultyForm from '@/components/FacultyForm';
 import { useAuth } from '@/providers/authProvider';
 import LoginPage from './auth/page';
