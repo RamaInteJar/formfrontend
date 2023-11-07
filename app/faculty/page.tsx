adba367be01ec200ed1fc9d3d0bd47b2dc19f741
pentiumKing <ramahawai87@gmail.com>
2023-11-07 07:07:35 -0500

    fixing bugs

--- app/faculty/page.tsx
+++ app/faculty/page.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { AuthProvider, useAuth } from '@/providers/authProvider';
-import { fetchFacultyForms } from '@/utils/apiCalls';
+import { AuthProvider, useAuth } from "@/providers/authProvider";
+import { fetchFacultyForms } from "@/utils/apiCalls";
 import {
   Table,
   TableHeader,
@@ -10,72 +10,72 @@ import {
   TableRow,
   TableCell,
   getKeyValue,
-} from '@nextui-org/react';
-import React, { useEffect, useState } from 'react';
+} from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
 
 const rows = [
   {
-    key: '1',
-    name: 'Tony Reichert',
-    role: 'CEO',
-    status: 'Active',
+    key: "1",
+    name: "Tony Reichert",
+    role: "CEO",
+    status: "Active",
   },
   {
-    key: '2',
-    name: 'Zoey Lang',
-    role: 'Technical Lead',
-    status: 'Paused',
+    key: "2",
+    name: "Zoey Lang",
+    role: "Technical Lead",
+    status: "Paused",
   },
   {
-    key: '3',
-    name: 'Jane Fisher',
-    role: 'Senior Developer',
-    status: 'Active',
+    key: "3",
+    name: "Jane Fisher",
+    role: "Senior Developer",
+    status: "Active",
   },
   {
-    key: '4',
-    name: 'William Howard',
-    role: 'Community Manager',
-    status: 'Vacation',
+    key: "4",
+    name: "William Howard",
+    role: "Community Manager",
+    status: "Vacation",
   },
 ];
 
 const columns = [
   {
-    key: 'faculty_id',
-    label: 'FACULTY ID',
+    key: "faculty_id",
+    label: "FACULTY ID",
   },
   {
-    key: 'users_ct',
-    label: 'USER',
+    key: "users_ct",
+    label: "USER",
   },
   {
-    key: 'duty',
-    label: 'DUTY',
+    key: "duty",
+    label: "DUTY",
   },
   {
-    key: 'reason',
-    label: 'REASON',
+    key: "reason",
+    label: "REASON",
   },
   {
-    key: 'sub_needed',
-    label: 'SUBNEEDED',
+    key: "sub_needed",
+    label: "SUBNEEDED",
   },
   {
-    key: 'times',
-    label: 'TIME',
+    key: "times",
+    label: "TIME",
   },
   {
-    key: 'after_school',
-    label: 'AFTER SCHOOL',
+    key: "after_school",
+    label: "AFTER SCHOOL",
   },
   {
-    key: 'full_day',
-    label: 'FULL DAY',
+    key: "full_day",
+    label: "FULL DAY",
   },
   {
-    key: 'status',
-    label: 'STATUS',
+    key: "status",
+    label: "STATUS",
   },
 ];
 
@@ -96,25 +96,27 @@ const FacultyPage = () => {
     fetchData();
   }, [accessToken]);
 
-  console.log('form', forms);
-
   return (
-      <Table aria-label="Example table with dynamic content">
-        <TableHeader columns={columns}>
-          {(column) => (
-            <TableColumn key={column.key}>{column.label}</TableColumn>
-          )}
-        </TableHeader>
-        <TableBody items={forms}>
-          {(item) => (
+    <Table aria-label="Example table with dynamic content">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={forms}>
+        {forms ? (
+          (item) => (
             <TableRow key={item}>
               {(columnKey) => (
                 <TableCell>{getKeyValue(item, columnKey)}</TableCell>
               )}
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
+          )
+        ) : (
+          <TableRow>
+            <TableCell>Not found</TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
   );
 };
 
