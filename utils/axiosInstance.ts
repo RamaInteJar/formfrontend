93450ef9f2901f4f108aa761e757ea8112ee4c16
Yusuph Omari <yousufummar649@gmail.com>
2024-09-07 13:00:54 +0300

    negatiate check

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -2,5 +2,5 @@ import { devBaseUrl, prodBaseUrl } from "@/config/apiConfig";
 import axios from "axios";
 
 export const axisoInstance = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? devBaseUrl : prodBaseUrl,
+  baseURL: process.env.NODE_ENV !== "development" ? devBaseUrl : prodBaseUrl,
 });
